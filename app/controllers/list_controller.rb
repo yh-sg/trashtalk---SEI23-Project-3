e3ccab0c706922331f8d385b61426fc34f3ff42f
metildachee <metildachee@gmail.com>
2020-08-08 11:51:16 +0800

    added List controller

---
+++ app/controllers/list_controller.rb
@@ -0,0 +1,10 @@
+class ListController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   
+  get 'list/index'
+  get 'list/show'
+  get 'list/new'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'users#index'
