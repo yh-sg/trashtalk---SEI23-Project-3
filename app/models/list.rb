d0165ce86542082aa7a792827984a392b4111783
metildachee <metildachee@gmail.com>
2020-08-09 14:16:37 +0800

    add List validation

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,16 @@
 class List < ApplicationRecord
+  validates :address, :presence => true,
+  :length => { :minimum => 5 }
+
+  validate :pickupdate_cannot_be_in_the_past
+
   belongs_to :user
   has_many :types
   accepts_nested_attributes_for :types
+
+  def pickupdate_cannot_be_in_the_past
+    if pickUpDate.present? && pickUpDate < Date.today
+      errors.add(:pickUpDate, "can't be in the past")
+    end
+  end  
 end
