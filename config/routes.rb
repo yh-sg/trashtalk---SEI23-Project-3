30b8c898f2a14af6bd76d2d2f15e8a290406ab1a
metildachee <metildachee@gmail.com>
2020-08-09 14:33:14 +0800

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :lists # /list/index 
-
+  get "users/:id/index", to: "users#index", as: :home #user/index
   get "users/:id", to: "users#show", as: :user # user/show
-
+  
   # for fufillment
   patch "/list/:id/assign", to: "lists#assign", as: :assign_list
   patch "/list/:id/complete", to: "lists#complete", as: :complete_list
