9a3875fb431e7987b4c805c90aab5d5f4d9f5888
Nur Amirah Sham <nuramirahsham@gmail.com>
2020-08-09 14:08:33 +0800

    db/scheme changed

--- db/schema.rb
+++ db/schema.rb
@@ -63,13 +63,13 @@ ActiveRecord::Schema.define(version: 2020_08_09_033056) do
     t.string "encrypted_password", default: "", null: false
     t.string "username", null: false
     t.string "address", null: false
-    t.integer "phone_number", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "role"
+    t.integer "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
