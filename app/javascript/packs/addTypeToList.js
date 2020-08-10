510dc4c59a122b7dd167869f45774a6c1edfe33e
yonghan <wangyh.92@gmail.com>
2020-08-10 16:52:12 +0800

    check on recycler

--- app/javascript/packs/addTypeToList.js
+++ app/javascript/packs/addTypeToList.js
@@ -128,7 +128,7 @@ const generateTypeInputForm = (index, container) => {
     // Adding remove button
     const removeBtn = generateDomElement("button")
     removeBtn.classList.add("btn");
-    removeBtn.classList.add("btn-outline-warning");
+    removeBtn.classList.add("btn-outline-danger");
     removeBtn.classList.add("mb-2");
     removeBtn.textContent = "Remove"
     removeBtn.addEventListener('click', removeContainer)
