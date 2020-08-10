0b07042b4d8f3461c9e25be7b77abba5d477c2f5
metildachee <metildachee@gmail.com>
2020-08-10 16:35:14 +0800

    added lat, log to List model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_09_033056) do
+ActiveRecord::Schema.define(version: 2020_08_10_083433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 2020_08_09_033056) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "collector"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
